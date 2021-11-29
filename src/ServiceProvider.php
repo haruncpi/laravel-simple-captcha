51c1aa0a9431031807cd3684d07704b5570ba68f
MD.HARUN-UR-RASHID <harun.cox@gmail.com>
2021-11-29 10:54:22 +0600

    refactor

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,23 +1,18 @@
-<?php
-
-namespace Haruncpi\LaravelSimpleCaptcha;
+<?php namespace Haruncpi\LaravelSimpleCaptcha;
 
 use Illuminate\Support\Facades\Validator;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-   
-
     public function boot()
     {
-        Validator::extend('simple_captcha', function($attribute, $value, $parameters)
-        {
+        Validator::extend('simple_captcha', function ($attribute, $value, $parameters) {
             return $value == SimpleCaptcha::getAnswer();
-        },'Invalid captcha answer');
+        }, 'Invalid captcha answer');
     }
 
     public function register()
     {
-       
+
     }
 }
--- src/SimpleCaptcha.php
+++ src/SimpleCaptcha.php
@@ -1,47 +1,43 @@
-<?php
-namespace Haruncpi\LaravelSimpleCaptcha;
+<?php namespace Haruncpi\LaravelSimpleCaptcha;
 
-use Session;
+use Illuminate\Support\Facades\Session;
 
-class SimpleCaptcha{
+class SimpleCaptcha
+{
     private $num1;
     private $num2;
     private $actionNumber;
     private $arr;
     private $sessionKey = '_simple_captcha';
-    
+
 
     private function generate()
     {
         $this->num1 = rand(2, 5);
         $this->num2 = rand(6, 9);
         $this->actionNumber = rand(1, 3);
-        
-        $this->ar = [];
+
         $this->arr['num1'] = $this->num1;
         $this->arr['num2'] = $this->num2;
 
-
-        switch ($this->actionNumber)
-        {
+        switch ($this->actionNumber) {
             case 1:
                 $this->arr['question'] = $this->num1 . " + " . $this->num2 . " = ? ";
                 $this->arr['answer'] = $this->addition($this->num1, $this->num2);
-                return $this->arr;
                 break;
 
             case 2:
                 $this->arr['question'] = $this->num2 . " - " . $this->num1 . " = ? ";
-                $this->arr['answer'] = $this->substruction($this->num1, $this->num2);
-                return $this->arr;
+                $this->arr['answer'] = $this->subtraction($this->num1, $this->num2);
                 break;
 
             case 3:
-                $this->arr['question'] = $this->num1 . " * " . $this->num2 . " = ? ";
+                $this->arr['question'] = $this->num1 . " x " . $this->num2 . " = ? ";
                 $this->arr['answer'] = $this->multiplication($this->num1, $this->num2);
-                return $this->arr;
                 break;
         }
+
+        return $this->arr;
     }
 
     private function addition($num1, $num2)
@@ -49,7 +45,7 @@ class SimpleCaptcha{
         return $num1 + $num2;
     }
 
-    private function substruction($num1, $num2)
+    private function subtraction($num1, $num2)
     {
         return $num2 - $num1;
     }
@@ -60,11 +56,10 @@ class SimpleCaptcha{
     }
 
 
-
     public static function getQuestion()
     {
         $instance = new self();
-        Session::put($instance->sessionKey,$instance->generate());
+        Session::put($instance->sessionKey, $instance->generate());
 
         return Session::get($instance->sessionKey)['question'];
     }
